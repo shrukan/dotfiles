061bcdb419214f6eb8d034021af98b6453bffaeb
shrukan <colin.dirren@gmail.com>
2026-05-01 20:30:31 +0200

    fix(dap): use which-key for defining keymaps

--- lua/config/plugins/dap/nvim-dap-view.lua
+++ lua/config/plugins/dap/nvim-dap-view.lua
@@ -5,26 +5,28 @@ return {
 		---@module 'dap-view'
 		---@type dapview.Config
 		opts = {},
-		keys = {
-			{
-				"<leader>dv",
-				function()
-					require("dap-view").toggle()
-				end,
-				desc = "Toggle view",
-			},
-			{
-				"<leader>dw",
-				"<cmd>DapViewWatch<cr>",
-				desc = "Watch expression",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>dW",
-				":DapViewWatch ",
-				desc = "Watch custom expression",
-				mode = { "n" },
-			},
-		},
+		config = function()
+			local wk = require("which-key")
+			wk.add({
+				{
+					"<leader>dv",
+					"<cmd>DapViewToggle<cr>",
+					desc = "Toggle view",
+					mode = { "n" },
+				},
+				{
+					"<leader>dw",
+					"<cmd>DapViewWatch<cr>",
+					desc = "Watch expression",
+					mode = { "n", "v" },
+				},
+				{
+					"<leader>dW",
+					":DapViewWatch ",
+					desc = "Watch custom expression",
+					mode = { "n" },
+				},
+			})
+		end,
 	},
 }
